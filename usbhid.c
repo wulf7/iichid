72f2274afab060a27bda26488852ebd19eb029eb
Vladimir Kondratyev <vladimir@kondratyev.su>
2019-09-20 01:25:59 +0300

    Allow usbhid to attach to any USB devices except keyboards

--- usbhid.c
+++ usbhid.c
@@ -675,8 +675,6 @@ uhid_probe(device_t dev)
 {
 	struct usb_attach_arg *uaa = device_get_ivars(dev);
 	int error;
-	void *buf;
-	uint16_t len;
 
 	DPRINTFN(11, "\n");
 
@@ -698,29 +696,8 @@ uhid_probe(device_t dev)
 	if ((uaa->info.bInterfaceClass == UICLASS_HID) &&
 	    (uaa->info.bInterfaceSubClass == UISUBCLASS_BOOT) &&
 	    (((uaa->info.bInterfaceProtocol == UIPROTO_BOOT_KEYBOARD) &&
-	      !usb_test_quirk(uaa, UQ_KBD_IGNORE)) ||
-	     ((uaa->info.bInterfaceProtocol == UIPROTO_MOUSE) &&
-	      !usb_test_quirk(uaa, UQ_UMS_IGNORE))))
+	      !usb_test_quirk(uaa, UQ_KBD_IGNORE))))
 		return (ENXIO);
-
-	/* Check for mandatory multitouch usages to give wmt(4) a chance */
-	if (!usb_test_quirk(uaa, UQ_WMT_IGNORE)) {
-		error = usbd_req_get_hid_desc(uaa->device, NULL,
-		    &buf, &len, M_USBDEV, uaa->info.bIfaceIndex);
-		/* Let HID decscriptor-less devices to be handled at attach */
-		if (!error) {
-			if (hid_locate(buf, len,
-			    HID_USAGE2(HUP_DIGITIZERS, HUD_CONTACT_MAX),
-			    hid_feature, 0, NULL, NULL, NULL) &&
-			    hid_locate(buf, len,
-			    HID_USAGE2(HUP_DIGITIZERS, HUD_CONTACTID),
-			    hid_input, 0, NULL, NULL, NULL)) {
-				free(buf, M_USBDEV);
-				return (ENXIO);
-			}
-			free(buf, M_USBDEV);
-		}
-	}
 
 	return (BUS_PROBE_GENERIC);
 }
