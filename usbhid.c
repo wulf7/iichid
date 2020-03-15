585a115e35ebf8435066ba893db2349a4e650c42
Vladimir Kondratyev <vladimir@kondratyev.su>
2020-03-16 02:41:59 +0300

    [usbhid] Run get_report command with USB xfer callback

--- usbhid.c
+++ usbhid.c
@@ -96,9 +96,7 @@ enum {
 	USBHID_INTR_DT_WR,
 	USBHID_INTR_DT_RD,
 	USBHID_CTRL_DT_WR,
-#ifdef NOT_YET
 	USBHID_CTRL_DT_RD,
-#endif
 	USBHID_N_TRANSFER,
 };
 
@@ -151,9 +149,7 @@ static device_detach_t usbhid_detach;
 static usb_callback_t usbhid_write_callback;
 static usb_callback_t usbhid_read_callback;
 static usb_callback_t usbhid_set_report_callback;
-#ifdef NOT_YET
 static usb_callback_t usbhid_get_report_callback;
-#endif
 
 static void
 usbhid_write_callback(struct usb_xfer *xfer, usb_error_t error)
@@ -252,7 +248,6 @@ usbhid_fill_set_report(struct usb_device_request *req, uint8_t iface_no,
 	USETW(req->wLength, size);
 }
 
-#ifdef NOT_YET
 static void
 usbhid_fill_get_report(struct usb_device_request *req, uint8_t iface_no,
     uint8_t type, uint8_t id, uint16_t size)
@@ -264,7 +259,6 @@ usbhid_fill_get_report(struct usb_device_request *req, uint8_t iface_no,
 	req->wIndex[1] = 0;
 	USETW(req->wLength, size);
 }
-#endif
 
 static void
 usbhid_set_report_callback(struct usb_xfer *xfer, usb_error_t error)
@@ -307,46 +301,44 @@ tr_exit:
 	}
 }
 
-#ifdef NOT_YET
 static void
 usbhid_get_report_callback(struct usb_xfer *xfer, usb_error_t error)
 {
 	struct usbhid_softc *sc = usbd_xfer_softc(xfer);
-	struct usb_device_request req;
 	struct usb_page_cache *pc;
 
 	pc = usbd_xfer_get_frame(xfer, 0);
 
 	switch (USB_GET_STATE(xfer)) {
-	case USB_ST_TRANSFERRED:
-		usb_fifo_put_data(sc->sc_fifo.fp[USB_FIFO_RX], pc, sizeof(req),
-		    sc->sc_isize, 1);
-		return;
-
 	case USB_ST_SETUP:
-
-		if (usb_fifo_put_bytes_max(sc->sc_fifo.fp[USB_FIFO_RX]) > 0) {
-
-			usbhid_fill_get_report
-			    (&req, sc->sc_iface_no, UHID_INPUT_REPORT,
-			    sc->sc_iid, sc->sc_isize);
-
-			usbd_copy_in(pc, 0, &req, sizeof(req));
-
-			usbd_xfer_set_frame_len(xfer, 0, sizeof(req));
-			usbd_xfer_set_frame_len(xfer, 1, sc->sc_isize);
-			usbd_xfer_set_frames(xfer, sc->sc_isize ? 2 : 1);
-			usbd_transfer_submit(xfer);
+		if (sc->sc_tr_len > usbd_xfer_max_len(xfer)) {
+			sc->sc_tr_error = ENOBUFS;
+			goto tr_exit;
 		}
+
+		usbd_copy_in(pc, 0, sc->sc_tr_req, sizeof(*sc->sc_tr_req));
+		usbd_xfer_set_frame_len(xfer, 0, sizeof(*sc->sc_tr_req));
+		usbd_xfer_set_frame_len(xfer, 1, sc->sc_tr_len);
+		usbd_xfer_set_frames(xfer, sc->sc_isize ? 2 : 1);
+		usbd_transfer_submit(xfer);
 		return;
+
+	case USB_ST_TRANSFERRED:
+		usbd_copy_out(pc, sizeof(*sc->sc_tr_req), sc->sc_tr_buf,
+		    sc->sc_tr_len);
+		sc->sc_tr_error = 0;
+		goto tr_exit;
 
 	default:			/* Error */
 		/* bomb out */
-		usb_fifo_put_data_error(sc->sc_fifo.fp[USB_FIFO_RX]);
+		DPRINTFN(1, "error=%s\n", usbd_errstr(error));
+		sc->sc_tr_error = EIO;
+tr_exit:
+		if (!HID_IN_POLLING_MODE_FUNC())
+			wakeup(sc);
 		return;
 	}
 }
-#endif
 
 static const struct usb_config usbhid_config[USBHID_N_TRANSFER] = {
 
@@ -372,7 +364,6 @@ static const struct usb_config usbhid_config[USBHID_N_TRANSFER] = {
 		.callback = &usbhid_set_report_callback,
 		.timeout = 1000,	/* 1 second */
 	},
-#ifdef NOT_YET
 	[USBHID_CTRL_DT_RD] = {
 		.type = UE_CONTROL,
 		.endpoint = 0x00,	/* Control pipe */
@@ -381,7 +372,6 @@ static const struct usb_config usbhid_config[USBHID_N_TRANSFER] = {
 		.callback = &usbhid_get_report_callback,
 		.timeout = 1000,	/* 1 second */
 	},
-#endif
 };
 
 static void
@@ -401,10 +391,8 @@ usbhid_intr_setup(device_t dev, struct mtx *mtx, hid_intr_t intr,
 	sc->sc_config[USBHID_INTR_DT_RD].bufsize = sc->sc_isize;
 	sc->sc_config[USBHID_CTRL_DT_WR].bufsize =
 	    MAX(sc->sc_osize, sc->sc_fsize);
-#ifdef NOT_YET
 	sc->sc_config[USBHID_CTRL_DT_RD].bufsize =
 	    MAX(sc->sc_isize, sc->sc_fsize);
-#endif
 
 	error = usbd_transfer_setup(sc->sc_udev,
 	    &sc->sc_iface_index, sc->sc_xfer, sc->sc_config,
@@ -509,17 +497,18 @@ usbhid_get_report(device_t dev, void *buf, uint16_t maxlen, uint16_t *actlen,
     uint8_t type, uint8_t id)
 {
 	struct usbhid_softc* sc = device_get_softc(dev);
-	int err;
+	struct usb_device_request req;
+	int error;
 
-	err = usbd_req_get_report(sc->sc_udev, NULL, buf,
-	    maxlen, sc->sc_iface_index, type, id);
-	if (err)
-                err = ENXIO;
-	else
-		if (actlen != NULL)
-			*actlen = maxlen;
+	usbhid_fill_get_report(&req, sc->sc_iface_no, type, id, maxlen);
 
-	return (err);
+	error = usbhid_sync_xfer
+	    (sc, sc->sc_xfer[USBHID_CTRL_DT_RD], &req, buf, maxlen);
+
+	if (!error && actlen != NULL)
+		*actlen = maxlen;
+
+	return (error);
 }
 
 static int
